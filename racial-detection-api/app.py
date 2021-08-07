9a4e36722fb46dfe08bc29d4b29af82ffdf1ee7a
Aryan Chavan <aryan@aryansdomain.com>
2021-08-07 10:31:37 +0530

    add racial detection

--- racial-detection-api/app.py
+++ racial-detection-api/app.py
@@ -2,6 +2,10 @@ import cv2
 from flask import Flask, request
 from os import path
 from random import randint
+from deepface import DeepFace
+from os import remove
+
+print('imported packages')
 
 app = Flask(__name__)
 
@@ -11,8 +15,24 @@ def detect_race():
     photo = request.files['photo']
     filename = path.join('uploads', str(randint(0, 4096)) + photo.filename)
     photo.save(filename)
-    
-    return {"race": "black"}
+
+    # reading image
+    try:
+        img = cv2.imread(filename)
+    except:
+        return {'error': 'invalid image'}
+
+    # deleting file
+    remove(filename)
+
+    # detecting race
+    try:
+        predictions = DeepFace.analyze(img, actions=['race'])
+    except:
+        return {'error': 'could not find face'}
+
+    # responding with predictions
+    return {"race": predictions}
 
 
 if __name__ == '__main__':
